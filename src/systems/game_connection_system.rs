1d20199eb685bfd348a6d9292f96095a8cabe567
James Benton <james.benton2@gmail.com>
2022-03-17 22:06:38 +0000

    Store the current player in client_entity_list.
    Easier than using a query.

--- src/systems/game_connection_system.rs
+++ src/systems/game_connection_system.rs
@@ -21,12 +21,16 @@ use crate::{
 
 pub struct ClientEntityList {
     pub client_entities: Vec<Option<Entity>>,
+    pub player_entity: Option<Entity>,
+    pub player_entity_id: Option<ClientEntityId>,
 }
 
 impl Default for ClientEntityList {
     fn default() -> Self {
         Self {
             client_entities: vec![None; 4096],
+            player_entity: None,
+            player_entity_id: None,
         }
     }
 }
@@ -55,7 +59,6 @@ pub fn game_connection_system(
     game_connection: Option<Res<GameConnection>>,
     mut loaded_zone: ResMut<LoadedZone>,
     mut app_state: ResMut<State<AppState>>,
-    query_player: Query<Entity, With<PlayerCharacter>>,
     mut client_entity_list: Local<ClientEntityList>,
 ) {
     if game_connection.is_none() {
@@ -76,86 +79,92 @@ pub fn game_connection_system(
                 loaded_zone.next_zone_id = Some(character_data.position.zone_id);
 
                 // Spawn character
-                commands
-                    .spawn_bundle((
-                        character_data.character_info,
-                        character_data.basic_stats,
-                        character_data.level,
-                        character_data.equipment,
-                        character_data.experience_points,
-                        character_data.skill_list,
-                        character_data.hotbar,
-                        character_data.health_points,
-                        character_data.mana_points,
-                        character_data.stat_points,
-                        character_data.skill_points,
-                        character_data.union_membership,
-                        character_data.stamina,
-                        character_data.position.clone(),
-                        StatusEffects::default(),
-                    ))
-                    .insert_bundle((
-                        PlayerCharacter {},
-                        Transform::from_xyz(
-                            character_data.position.position.x / 100.0,
-                            20.0,
-                            -character_data.position.position.y / 100.0,
-                        ),
-                        GlobalTransform::default(),
-                        Visibility::default(),
-                        ComputedVisibility::default(),
-                    ));
+                client_entity_list.player_entity = Some(
+                    commands
+                        .spawn_bundle((
+                            character_data.character_info,
+                            character_data.basic_stats,
+                            character_data.level,
+                            character_data.equipment,
+                            character_data.experience_points,
+                            character_data.skill_list,
+                            character_data.hotbar,
+                            character_data.health_points,
+                            character_data.mana_points,
+                            character_data.stat_points,
+                            character_data.skill_points,
+                            character_data.union_membership,
+                            character_data.stamina,
+                            character_data.position.clone(),
+                            StatusEffects::default(),
+                        ))
+                        .insert_bundle((
+                            PlayerCharacter {},
+                            Transform::from_xyz(
+                                character_data.position.position.x / 100.0,
+                                20.0,
+                                -character_data.position.position.y / 100.0,
+                            ),
+                            GlobalTransform::default(),
+                            Visibility::default(),
+                            ComputedVisibility::default(),
+                        ))
+                        .id(),
+                );
 
                 // Tell server we are ready to join the zone
+                // TODO: Do this after zone loading completes ?
                 game_connection
                     .client_message_tx
                     .send(ClientMessage::JoinZoneRequest)
                     .ok();
-
-                // We return immediately so future packets will be able to use query_player
-                return;
             }
             Ok(ServerMessage::CharacterDataItems(message)) => {
-                commands
-                    .entity(query_player.single())
-                    .insert_bundle((message.inventory, message.equipment));
+                if let Some(player_entity) = client_entity_list.player_entity {
+                    commands
+                        .entity(player_entity)
+                        .insert_bundle((message.inventory, message.equipment));
+                }
             }
             Ok(ServerMessage::CharacterDataQuest(message)) => {
-                commands
-                    .entity(query_player.single())
-                    .insert(message.quest_state);
+                if let Some(player_entity) = client_entity_list.player_entity {
+                    commands.entity(player_entity).insert(message.quest_state);
+                }
             }
             Ok(ServerMessage::JoinZone(message)) => {
-                let entity = query_player.single();
-                commands
-                    .entity(entity)
-                    .insert_bundle((
-                        ClientEntity::new(
-                            ClientEntityType::Character,
-                            message.entity_id,
-                            ZoneId::new(1).unwrap(), // TODO: Is ZoneId important in ClientEntity for client?
-                        ),
-                        message.experience_points,
-                        message.team,
-                        message.health_points,
-                        message.mana_points,
-                    ))
-                    .with_children(|child_builder| {
-                        child_builder.spawn_bundle((
-                            CollisionRayCastSource {},
-                            Transform::default()
-                                .with_translation(Vec3::new(0.0, 1.35, 0.0))
-                                .looking_at(-Vec3::Y, Vec3::X),
-                            GlobalTransform::default(),
-                        ));
-                    });
-                client_entity_list.clear();
-                client_entity_list.add(message.entity_id, entity);
-                // TODO: Do something with message.world_ticks
+                if let Some(player_entity) = client_entity_list.player_entity {
+                    commands
+                        .entity(player_entity)
+                        .insert_bundle((
+                            ClientEntity::new(
+                                ClientEntityType::Character,
+                                message.entity_id,
+                                ZoneId::new(1).unwrap(), // TODO: Is ZoneId important in ClientEntity for client?
+                            ),
+                            message.experience_points,
+                            message.team,
+                            message.health_points,
+                            message.mana_points,
+                        ))
+                        .with_children(|child_builder| {
+                            child_builder.spawn_bundle((
+                                CollisionRayCastSource {},
+                                Transform::default()
+                                    .with_translation(Vec3::new(0.0, 1.35, 0.0))
+                                    .looking_at(-Vec3::Y, Vec3::X),
+                                GlobalTransform::default(),
+                            ));
+                        });
 
-                // Transition to in game state if we are not already
-                if !matches!(app_state.current(), AppState::Game) {
-                    app_state.set(AppState::Game).ok();
+                    client_entity_list.clear();
+                    client_entity_list.add(message.entity_id, player_entity);
+                    client_entity_list.player_entity_id = Some(message.entity_id);
+                    // TODO: Do something with message.world_ticks
+
+                    // Transition to in game state if we are not already
+                    if !matches!(app_state.current(), AppState::Game) {
+                        app_state.set(AppState::Game).ok();
+                    }
                 }
             }
             Ok(ServerMessage::SpawnEntityNpc(message)) => {
