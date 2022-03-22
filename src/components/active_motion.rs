9b30b6c57b48b4b402573555d6ff2350b7a35e10
James Benton <james.benton2@gmail.com>
2022-03-22 21:33:07 +0000

    Add support for camera animation to animation system.

--- src/components/active_motion.rs
+++ src/components/active_motion.rs
@@ -6,10 +6,22 @@ use crate::zmo_asset_loader::ZmoAsset;
 pub struct ActiveMotion {
     pub start_time: f64,
     pub motion: Handle<ZmoAsset>,
+    pub repeat_limit: Option<usize>, // If None, repeats forever
+    pub complete: bool,
 }
 
 impl ActiveMotion {
     pub fn new(motion: Handle<ZmoAsset>, start_time: f64) -> Self {
-        Self { motion, start_time }
+        Self {
+            motion,
+            start_time,
+            repeat_limit: None,
+            complete: false,
+        }
+    }
+
+    pub fn with_repeat_limit(mut self, repeat_limit: usize) -> Self {
+        self.repeat_limit = Some(repeat_limit);
+        self
     }
 }
--- src/systems/animation_system.rs
+++ src/systems/animation_system.rs
@@ -1,21 +1,29 @@
 use bevy::{
     core::Time,
-    prelude::{Assets, Entity, Query, Res, Transform, Without},
-    render::mesh::skinning::SkinnedMesh,
+    math::Vec3,
+    prelude::{Assets, Commands, Entity, PerspectiveProjection, Query, Res, Transform},
+    render::{camera::Camera3d, mesh::skinning::SkinnedMesh},
 };
 
 use crate::{components::ActiveMotion, zmo_asset_loader::ZmoAsset};
 
+#[allow(clippy::type_complexity)]
 pub fn animation_system(
+    mut commands: Commands,
     mut query_transform: Query<&mut Transform>,
-    mut query_skinned: Query<(&ActiveMotion, &SkinnedMesh)>,
-    mut query_unskinned: Query<(Entity, &ActiveMotion), Without<SkinnedMesh>>,
+    mut query_projection: Query<&mut PerspectiveProjection>,
+    query_active_motions: Query<(
+        Entity,
+        &ActiveMotion,
+        Option<&SkinnedMesh>,
+        Option<&Camera3d>,
+    )>,
     motion_assets: Res<Assets<ZmoAsset>>,
     time: Res<Time>,
 ) {
     let current_time = time.seconds_since_startup();
 
-    for (active_motion, skinned_mesh) in query_skinned.iter_mut() {
+    for (entity, active_motion, skinned_mesh, camera_3d) in query_active_motions.iter() {
         let current_motion = motion_assets.get(&active_motion.motion);
         if current_motion.is_none() {
             continue;
@@ -25,56 +33,107 @@ pub fn animation_system(
         let current_animation_time = current_time - active_motion.start_time;
         let current_frame_index_exact = current_animation_time * (current_motion.fps() as f64);
         let current_frame_fract = current_frame_index_exact.fract() as f32;
+        let current_loop_count = current_frame_index_exact as usize / current_motion.num_frames();
+        if current_loop_count >= active_motion.repeat_limit.unwrap_or(usize::MAX) {
+            commands.entity(entity).remove::<ActiveMotion>();
+            continue;
+        }
+
         let current_frame_index = current_frame_index_exact as usize % current_motion.num_frames();
+        let next_frame_index = if current_frame_index + 1 == current_motion.num_frames()
+            && current_loop_count + 1 >= active_motion.repeat_limit.unwrap_or(usize::MAX)
+        {
+            // The last frame of last loop should not blend to the first frame
+            current_frame_index
+        } else {
+            (current_frame_index + 1) % current_motion.num_frames()
+        };
 
-        // TODO: Support non-looping animations by making next frame optional
-        let next_frame_index = (current_frame_index + 1) % current_motion.num_frames();
+        if let Some(skinned_mesh) = skinned_mesh {
+            for (bone_id, bone_entity) in skinned_mesh.joints.iter().enumerate() {
+                if let Ok(mut bone_transform) = query_transform.get_mut(*bone_entity) {
+                    let current_frame_translation =
+                        current_motion.get_translation(bone_id, current_frame_index);
+                    let next_frame_translation =
+                        current_motion.get_translation(bone_id, next_frame_index);
 
-        for (bone_id, bone_entity) in skinned_mesh.joints.iter().enumerate() {
-            if let Ok(mut bone_transform) = query_transform.get_mut(*bone_entity) {
-                let current_frame_translation =
-                    current_motion.get_translation(bone_id, current_frame_index);
-                let next_frame_translation =
-                    current_motion.get_translation(bone_id, next_frame_index);
+                    if let (Some(current_frame_translation), Some(next_frame_translation)) =
+                        (current_frame_translation, next_frame_translation)
+                    {
+                        bone_transform.translation = current_frame_translation
+                            .lerp(next_frame_translation, current_frame_fract);
+                    }
 
-                if let (Some(current_frame_translation), Some(next_frame_translation)) =
-                    (current_frame_translation, next_frame_translation)
-                {
-                    bone_transform.translation =
-                        current_frame_translation.lerp(next_frame_translation, current_frame_fract);
+                    let current_frame_rotation =
+                        current_motion.get_rotation(bone_id, current_frame_index);
+                    let next_frame_rotation =
+                        current_motion.get_rotation(bone_id, next_frame_index);
+                    if let (Some(current_frame_rotation), Some(next_frame_rotation)) =
+                        (current_frame_rotation, next_frame_rotation)
+                    {
+                        bone_transform.rotation =
+                            current_frame_rotation.lerp(next_frame_rotation, current_frame_fract);
+                    }
+
+                    // TODO: Skinned mesh also support animation of Alpha and UV
                 }
-
-                let current_frame_rotation =
-                    current_motion.get_rotation(bone_id, current_frame_index);
-                let next_frame_rotation = current_motion.get_rotation(bone_id, next_frame_index);
-                if let (Some(current_frame_rotation), Some(next_frame_rotation)) =
-                    (current_frame_rotation, next_frame_rotation)
-                {
-                    bone_transform.rotation =
-                        current_frame_rotation.lerp(next_frame_rotation, current_frame_fract);
-                }
-
-                // TODO: Skinned mesh also support animation of Alpha and UV
             }
-        }
-    }
+        } else if camera_3d.is_some() {
+            let current_eye = current_motion.get_translation(0, current_frame_index);
+            let next_eye = current_motion.get_translation(0, next_frame_index);
+            let eye = if let (Some(current_eye), Some(next_eye)) = (current_eye, next_eye) {
+                Some(
+                    current_eye.lerp(next_eye, current_frame_fract)
+                        + Vec3::new(5200.0, 0.0, -5200.0),
+                )
+            } else {
+                None
+            };
 
-    for (entity, active_motion) in query_unskinned.iter_mut() {
-        let current_motion = motion_assets.get(&active_motion.motion);
-        if current_motion.is_none() {
-            continue;
-        }
-        let current_motion = current_motion.unwrap();
+            let current_center = current_motion.get_translation(1, current_frame_index);
+            let next_center = current_motion.get_translation(1, next_frame_index);
+            let center =
+                if let (Some(current_center), Some(next_center)) = (current_center, next_center) {
+                    Some(
+                        current_center.lerp(next_center, current_frame_fract)
+                            + Vec3::new(5200.0, 0.0, -5200.0),
+                    )
+                } else {
+                    None
+                };
 
-        let current_animation_time = current_time - active_motion.start_time;
-        let current_frame_index_exact = current_animation_time * (current_motion.fps() as f64);
-        let current_frame_fract = current_frame_index_exact.fract() as f32;
-        let current_frame_index = current_frame_index_exact as usize % current_motion.num_frames();
+            let current_up = current_motion.get_translation(2, current_frame_index);
+            let next_up = current_motion.get_translation(2, next_frame_index);
+            let up = if let (Some(current_up), Some(next_up)) = (current_up, next_up) {
+                Some(current_up.lerp(next_up, current_frame_fract))
+            } else {
+                None
+            };
 
-        // TODO: Support non-looping animations by making next frame optional
-        let next_frame_index = (current_frame_index + 1) % current_motion.num_frames();
+            let current_fov_near_far = current_motion.get_translation(3, current_frame_index);
+            let next_fov_near_far = current_motion.get_translation(3, next_frame_index);
+            let fov_near_far = if let (Some(current_fov_near_far), Some(next_fov_near_far)) =
+                (current_fov_near_far, next_fov_near_far)
+            {
+                Some(current_fov_near_far.lerp(next_fov_near_far, current_frame_fract))
+            } else {
+                None
+            };
 
-        if let Ok(mut entity_transform) = query_transform.get_mut(entity) {
+            if let (Some(eye), Some(center), Some(up)) = (eye, center, up) {
+                if let Ok(mut transform) = query_transform.get_mut(entity) {
+                    *transform = Transform::from_translation(eye).looking_at(center, up);
+                }
+            }
+
+            if let Some(fov_near_far) = fov_near_far {
+                if let Ok(mut perspective_projection) = query_projection.get_mut(entity) {
+                    perspective_projection.fov = (fov_near_far.x * 100.0).to_radians();
+                    perspective_projection.near = -fov_near_far.z;
+                    perspective_projection.far = fov_near_far.y * 10.0;
+                }
+            }
+        } else if let Ok(mut entity_transform) = query_transform.get_mut(entity) {
             let current_frame_translation = current_motion.get_translation(0, current_frame_index);
             let next_frame_translation = current_motion.get_translation(0, next_frame_index);
 
