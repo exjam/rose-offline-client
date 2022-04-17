da1248b287af34580f07e0e76cffc82335e4f942
James Benton <james.benton2@gmail.com>
2022-04-17 20:09:08 +0100

    Spawn projectile when using ranged weapon.

--- src/systems/animation_effect_system.rs
+++ src/systems/animation_effect_system.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::{Entity, EventReader, EventWriter, Query, Res};
 
-use rose_data::{AnimationEventFlags, EffectBulletMoveType, EquipmentIndex, SkillData, SkillType};
+use rose_data::{
+    AmmoIndex, AnimationEventFlags, EffectBulletMoveType, EquipmentIndex, ItemClass, SkillData,
+    SkillType,
+};
 use rose_game_common::components::{Equipment, MoveSpeed, Npc};
 
 use crate::{
@@ -59,6 +62,62 @@ pub fn animation_effect_system(
                         command_attack.target,
                         SpawnEffectData::with_file_id(hit_effect_file_id),
                     ));
+                }
+            }
+        }
+
+        if event
+            .flags
+            .contains(AnimationEventFlags::EFFECT_WEAPON_FIRE_BULLET)
+        {
+            if let Ok(Command::Attack(command_attack)) = query_command.get(event.entity) {
+                let projectile_effect_data = query_equipment
+                    .get(event.entity)
+                    .ok()
+                    .and_then(|equipment| {
+                        game_data
+                            .items
+                            .get_weapon_item(
+                                equipment
+                                    .get_equipment_item(EquipmentIndex::WeaponRight)
+                                    .map(|weapon| weapon.item.item_number)
+                                    .unwrap_or(0),
+                            )
+                            .and_then(|weapon_item_data| {
+                                match weapon_item_data.item_data.class {
+                                    ItemClass::Bow | ItemClass::Crossbow => Some(AmmoIndex::Arrow),
+                                    ItemClass::Gun | ItemClass::DualGuns => Some(AmmoIndex::Bullet),
+                                    ItemClass::Launcher => Some(AmmoIndex::Throw),
+                                    _ => None,
+                                }
+                                .and_then(|ammo_index| equipment.get_ammo_item(ammo_index))
+                                .and_then(|ammo_item| {
+                                    game_data
+                                        .items
+                                        .get_material_item(ammo_item.item.item_number)
+                                })
+                                .and_then(|ammo_item_data| ammo_item_data.bullet_effect_id)
+                                .or(weapon_item_data.bullet_effect_id)
+                            })
+                    })
+                    .and_then(|id| game_data.effect_database.get_effect(id));
+
+                if let Some(projectile_effect_data) = projectile_effect_data {
+                    if let Some(projectile_effect_file_id) = projectile_effect_data.bullet_normal {
+                        spawn_projectile_events.send(SpawnProjectileEvent {
+                            source: event.entity,
+                            source_dummy_bone_id: Some(0),
+                            target: SpawnProjectileTarget::Entity(command_attack.target),
+                            move_type: projectile_effect_data
+                                .bullet_move_type
+                                .as_ref()
+                                .cloned()
+                                .unwrap_or(EffectBulletMoveType::Linear),
+                            move_speed: MoveSpeed::new(projectile_effect_data.bullet_speed / 100.0),
+                            projectile_effect_file_id: Some(projectile_effect_file_id),
+                            hit_effect_file_id: projectile_effect_data.hit_normal, // TODO: .hit_critical
+                        });
+                    }
                 }
             }
         }
