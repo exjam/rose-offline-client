2ee5931ba40ed01f019c67713d94557fefd2a50f
James Benton <james.benton2@gmail.com>
2022-09-06 21:53:15 +0100

    Improve debug ui dialog list to be ordered alphabetically and add filter.

--- src/ui/ui_debug_dialog_list.rs
+++ src/ui/ui_debug_dialog_list.rs
@@ -1,5 +1,9 @@
-use bevy::prelude::{AssetServer, Assets, Handle, Local, Res, ResMut};
+use bevy::{
+    asset::HandleId,
+    prelude::{AssetServer, Assets, Handle, Local, Res, ResMut},
+};
 use bevy_egui::{egui, EguiContext};
+use regex::Regex;
 
 use crate::ui::{
     widgets::{DataBindings, Dialog},
@@ -8,16 +12,19 @@ use crate::ui::{
 
 #[derive(Default)]
 pub struct UiStateDebugDialogs {
-    pub draw_dialog: Option<Handle<Dialog>>,
+    draw_dialog: Option<Handle<Dialog>>,
+    filter_name: String,
+    filtered_dialogs: Vec<(String, HandleId)>,
 }
 
 pub fn ui_debug_dialog_list_system(
     mut egui_context: ResMut<EguiContext>,
     mut ui_state_debug_windows: ResMut<UiStateDebugWindows>,
-    mut ui_state_debug_dialogs: Local<UiStateDebugDialogs>,
+    mut ui_state: Local<UiStateDebugDialogs>,
     asset_server: Res<AssetServer>,
     dialog_assets: Res<Assets<Dialog>>,
 ) {
+    let ui_state = &mut *ui_state;
     if !ui_state_debug_windows.debug_ui_open {
         return;
     }
@@ -27,6 +34,53 @@ pub fn ui_debug_dialog_list_system(
         .default_height(300.0)
         .open(&mut ui_state_debug_windows.dialog_list_open)
         .show(egui_context.ctx_mut(), |ui| {
+            let mut filter_changed = false;
+
+            egui::Grid::new("dialog_list_controls_grid")
+                .num_columns(2)
+                .show(ui, |ui| {
+                    ui.label("Dialog Path Filter:");
+                    if ui.text_edit_singleline(&mut ui_state.filter_name).changed() {
+                        filter_changed = true;
+                    }
+                    ui.end_row();
+                });
+
+            if ui_state.filter_name.is_empty() && ui_state.filtered_dialogs.is_empty() {
+                filter_changed = true;
+            }
+
+            if filter_changed {
+                let filter_name_re = if !ui_state.filter_name.is_empty() {
+                    Some(
+                        Regex::new(&format!("(?i){}", regex::escape(&ui_state.filter_name)))
+                            .unwrap(),
+                    )
+                } else {
+                    None
+                };
+
+                ui_state.filtered_dialogs = dialog_assets
+                    .iter()
+                    .filter_map(|(handle_id, _)| {
+                        let asset_path = asset_server
+                            .get_handle_path(handle_id)?
+                            .path()
+                            .to_string_lossy()
+                            .to_string();
+                        if !filter_name_re
+                            .as_ref()
+                            .map_or(true, |re| re.is_match(&asset_path))
+                        {
+                            None
+                        } else {
+                            Some((asset_path, handle_id))
+                        }
+                    })
+                    .collect();
+                ui_state.filtered_dialogs.sort_by(|(a, _), (b, _)| a.cmp(b));
+            }
+
             egui_extras::TableBuilder::new(ui)
                 .striped(true)
                 .cell_layout(egui::Layout::left_to_right().with_cross_align(egui::Align::Center))
@@ -40,25 +94,23 @@ pub fn ui_debug_dialog_list_system(
                         ui.heading("Action");
                     });
                 })
-                .body(|mut body| {
-                    for (handle_id, _) in dialog_assets.iter() {
-                        body.row(20.0, |mut row| {
+                .body(|body| {
+                    body.rows(
+                        20.0,
+                        ui_state.filtered_dialogs.len(),
+                        |row_index, mut row| {
+                            let (path, handle_id) = &ui_state.filtered_dialogs[row_index];
                             row.col(|ui| {
-                                if let Some(asset_path) = asset_server.get_handle_path(handle_id) {
-                                    if let Some(str) = asset_path.path().to_str() {
-                                        ui.label(str);
-                                    }
-                                }
+                                ui.label(path);
                             });
 
                             row.col(|ui| {
                                 if ui.button("View").clicked() {
-                                    ui_state_debug_dialogs.draw_dialog =
-                                        Some(Handle::weak(handle_id));
+                                    ui_state.draw_dialog = Some(Handle::weak(*handle_id));
                                 }
                             });
-                        });
-                    }
+                        },
+                    );
                 });
         });
 
@@ -66,7 +118,7 @@ pub fn ui_debug_dialog_list_system(
         return;
     }
 
-    if let Some(dialog) = ui_state_debug_dialogs
+    if let Some(dialog) = ui_state
         .draw_dialog
         .as_ref()
         .and_then(|handle| dialog_assets.get(handle))
