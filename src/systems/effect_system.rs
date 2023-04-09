10b74ac87e4982f9f876f47ebb92ba813f58ca8d
James Benton <james.benton2@gmail.com>
2023-04-09 17:12:09 +0100

    Fix effect system to check completion for particle and mesh animations.

--- src/systems/effect_system.rs
+++ src/systems/effect_system.rs
@@ -3,14 +3,21 @@ use bevy::{
     prelude::{Commands, Entity, Query},
 };
 
-use crate::components::{Effect, EffectParticle, ParticleSequence};
+use crate::{
+    animation::{MeshAnimation, TransformAnimation},
+    components::{Effect, EffectMesh, EffectParticle, ParticleSequence},
+};
 
 pub fn effect_system(
     mut commands: Commands,
     query_effects: Query<(Entity, &Children, &Effect)>,
     query_children: Query<&Children>,
-    query_particle_sequence: Query<(&EffectParticle, &ParticleSequence)>,
-    // query_effect_mesh: Query<&EffectMesh>,
+    query_particle_sequence: Query<(
+        &EffectParticle,
+        &ParticleSequence,
+        Option<&TransformAnimation>,
+    )>,
+    query_effect_mesh: Query<(&EffectMesh, Option<&MeshAnimation>)>,
 ) {
     for (effect_entity, effect_children, effect) in query_effects.iter() {
         let mut children_finished = 0;
@@ -23,19 +30,29 @@ pub fn effect_system(
         for child in effect_children.iter() {
             if let Ok(children) = query_children.get(*child) {
                 for child in children.iter() {
-                    if let Ok((_, particle_sequence)) = query_particle_sequence.get(*child) {
-                        if particle_sequence.finished && particle_sequence.particles.is_empty() {
+                    if let Ok((_, particle_sequence, transform_animation)) =
+                        query_particle_sequence.get(*child)
+                    {
+                        let particle_sequence_completed =
+                            particle_sequence.finished && particle_sequence.particles.is_empty();
+                        let transform_animation_completed = transform_animation
+                            .map_or(true, |transform_animation| transform_animation.completed());
+
+                        if particle_sequence_completed || transform_animation_completed {
                             children_finished += 1;
                         } else {
                             children_running += 1;
                         }
                     }
 
-                    /*
-                    if let Ok(_) = query_effect_mesh.get(*child) {
-                        // TODO: Check if effect mesh is finished
+                    if let Ok((_, mesh_animation)) = query_effect_mesh.get(*child) {
+                        if mesh_animation.map_or(true, |mesh_animation| mesh_animation.completed())
+                        {
+                            children_finished += 1;
+                        } else {
+                            children_running += 1;
+                        }
                     }
-                    */
                 }
             }
         }
